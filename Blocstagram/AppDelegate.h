734008a6c17db1974f0206187e4c7ed194ecedfa
Brian Doore <doore.b@gmail.com>
2014-09-15 22:27:50 -0500

    Initial Commit

---
+++ Blocstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blocstagram
+//
+//  Created by Brian Doore on 9/15/14.
+//  Copyright (c) 2014 Brian Doore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
