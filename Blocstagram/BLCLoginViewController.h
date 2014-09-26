b7910def25d55f7474bb4e4e1e68dbdb57fc57f4
Brian Doore <doore.b@gmail.com>
2014-09-26 12:48:20 -0500

    Add instagram Login

---
+++ Blocstagram/BLCLoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  BLCLoginViewController.h
+//  Blocstagram
+//
+//  Created by Brian Doore on 9/26/14.
+//  Copyright (c) 2014 Brian Doore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCLoginViewController : UIViewController
+
+extern NSString *const BLCLoginViewControllerDidGetAccessTokenNotification;
+
+@end
